f539767b4ff6127e6b8b639235198f09464b6ac9
navneetraju <navneetraju66@gmail.com>
2025-04-17 17:28:39 -0700

    fix(scripts): update mount models script

--- scripts/mount_models.sh
+++ scripts/mount_models.sh
@@ -1,8 +1,11 @@
-rm -r ~/swarm_guard_models
-mkdir -p ~/swarm_guard_models
-fusermount -u ~/swarm_guard_models
+# ~/models exists delete it
+if [ -d ~/models ]; then
+  rm -rf ~/models
+fi
+mkdir -p ~/models
+fusermount -u ~/models
 gcsfuse \
   --stat-cache-ttl 0s \
   --type-cache-ttl 0s \
   --implicit-dirs \
-  swarm_guard_models ~/swarm_guard_models
+  swarm_guard_models ~/models
